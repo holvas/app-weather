141a09f548e1b672cb9d5f4d0f2645581bce0d23
Vasyl Holubovskyi <holvascompany@gmail.com>
2024-08-01 13:31:20 +0200

    update files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './GlobalStyled.js';
+import App from './components/App';
+import { GlobalStyles } from './GlobalStyled.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
